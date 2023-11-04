07ce79298b2f1a1a2dec508e10e4e818f47d54db
Laafer Issam <lissam@c4r9p3.1337.ma>
2023-11-04 13:09:26 +0100

    f

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: lissam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 09:46:04 by lissam            #+#    #+#             */
-/*   Updated: 2023/11/04 09:46:08 by lissam           ###   ########.fr       */
+/*   Updated: 2023/11/04 12:36:27 by lissam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,18 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	if (!string && !string2)
 		return (NULL);
 	mf = (unsigned char *)malloc(n);
-    if(!mf)
-        return NULL;
+	if (!mf)
+		return (NULL);
 	ft_memcpy(mf, string2, n);
 	ft_memcpy(string, mf, n);
 	return (dest);
 }
+// int main()
+// {
+//     char src[] = "adcdef";
+
+//     ft_memmove(src + 2, src, 3);
+//     printf("After memmove: %s\n", src);
+
+//     return 0;
+// }
