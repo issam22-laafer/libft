5b6dc6c593922dd02fcb7b56fffd978af28ed547
Laafer Issam <lissam@e1r9p11.1337.ma>
2023-11-08 21:32:33 +0100

    gg

--- ft_substr.c
+++ ft_substr.c
@@ -12,17 +12,33 @@
 
 #include "libft.h"
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
+char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char	*ptr;
-
-	if (!s)
-		return (NULL);
-	if (start > ft_strlen(s))
-		return (NULL);
-	ptr = (char *)malloc((len + 1) * sizeof(char));
-	ft_memmove(ptr, &s[start], len);
-	return (ptr);
+    char *ptr;
+    size_t i;
+    size_t j;
+    
+    i = start;
+    j = 0;
+    if(!s)
+        return (NULL);
+    ptr = malloc((len + 1) * sizeof(char));
+    if(!ptr)
+        return (NULL);
+    if(ft_strlen(s) <= start)
+    {
+        ptr[j] = '\0';
+        return(ptr);
+    }
+    while (s[i] != '\0' && len > 0)
+    {
+        ptr[j] = s[i];
+        i++;
+        j++;
+        len--;
+    }
+    ptr[j] = '\0';
+    return (ptr);
 }
 
 // int main()
