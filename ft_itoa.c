b816db400ea255df129f4ea4d1550ad9d5078e42
Laafer Issam <lissam@e1r13p9.1337.ma>
2023-11-11 17:47:41 +0100

    hh

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,8 +6,78 @@
 /*   By: lissam <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 12:17:17 by lissam            #+#    #+#             */
-/*   Updated: 2023/11/11 12:17:19 by lissam           ###   ########.fr       */
+/*   Updated: 2023/11/11 15:50:36 by lissam           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+static int	length_int(long int c)
+{
+	int	i;
+
+	i = 1;
+	if (c < 0)
+	{
+		i = i + 1;
+		c = -c;
+	}
+	while (c / 10)
+	{
+		i++;
+		c = c / 10;
+	}
+	return (i);
+}
+static void fill(char *p,long int nb, int len)
+{
+    while (nb)
+    {
+        p[len - 1] = (nb % 10) + 48;
+        nb = nb / 10;
+        len--;
+    }
+}
+
+char	*ft_itoa(int n)
+{
+	int		len;
+	int		i;
+	char	*p;
+	long int nb;
+
+	nb = n;
+	len = length_int(nb);
+	p = (char *)malloc((len + 1) * sizeof(char));
+	if (!p)
+		return (NULL);
+	i = 0;
+    if(nb == 0)
+        p[i] = 48;
+	if (nb < 0)
+	{
+		p[i] = '-';
+		i++;
+		nb = -nb;
+	}
+	p[len] = '\0';
+	fill(p,nb, len);
+	// while (nb)
+	// {
+	// 	p[len - 1] = (nb % 10) + 48;
+	// 	nb = nb / 10;
+	// 	len--;
+	// }
+	return (p);
+}
+// int main(void)
+// {
+//     int a = -2147483648;
+//     char *result = ft_itoa(a);
+//     if (result)
+//     {
+//         printf("the number: %s\n", result);
+//         free(result); // Don't forget to free the allocated memory
+//     }
+//     return 0;
+// }
--- libft.h
+++ libft.h
@@ -53,4 +53,5 @@ void    ft_putendl_fd(char *s, int fd);
 void    ft_putnbr_fd(int n, int fd);
 char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void    ft_striteri(char *s, void (*f)(unsigned int, char*));
+char	*ft_itoa(int n);
 #endif
