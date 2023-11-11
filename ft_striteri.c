e1816e878205cd1a942d614e079cfc8080eaef18
Laafer Issam <lissam@e1r13p9.1337.ma>
2023-11-11 11:42:44 +0100

    hh

---
+++ ft_striteri.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_striteri.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lissam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/11 11:14:29 by lissam            #+#    #+#             */
+/*   Updated: 2023/11/11 11:14:32 by lissam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    unsigned int i;
+
+    i = 0;
+    if(!s || !f)
+        return;
+    while (s[i])
+    {
+        f(i,&s[i]);
+        i++;
+    }
+}
+
+// void fupeer(unsigned int i, char *c)
+// {
+//     if(*c >= 97 && *c <= 122)
+//             *c = *c - 32;
+// }
+
+// int main()
+// {
+//     char str[] = "issam laafar";
+//     ft_striteri(str,fupeer);
+//     printf("%s",str);
+// }
