f4ae6ac307f5c05ee7915de5723bde79173520d6
Laafer Issam <lissam@e1r13p9.1337.ma>
2023-11-15 14:05:27 +0100

    free

---
+++ ft_lstdelone.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lissam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/15 12:49:49 by lissam            #+#    #+#             */
+/*   Updated: 2023/11/15 12:49:53 by lissam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void ft_lstdelone(t_list *lst, void (*del)(void*))
+{
+    if(lst && del)
+    {
+        del(lst->content);
+        free(lst);
+    }
+
+}
+// void del_int_data(void *data) {
+//     free(data);
+// }
+
+// int main() {
+//     t_list *node = (t_list *)malloc(sizeof(t_list));
+//     node->data = 42;
+//     node->next = NULL;
+//     ft_lstdelone(node, del_int_data);
+//     return 0;
+// }
